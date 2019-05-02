05c2e4f0422e7050aef863529a06ed93052fd27f
Gafurul (Rafi) Islam Kazi <gik211@nyu.edu>
2019-05-02 11:31:12 -0400

    Cleaned up and modified turns
    Got rid of unneeded comments
    
    Simplified the extinguish() function
    
    Modified the turns to turn in place

--- firebot.ino
+++ firebot.ino
@@ -1,17 +1,17 @@
 //Ultrasonic Sensor
 int ultSonicOutPin = 7;
 int ultSonicInPin = 10;
-int ultSonicThresh = 25; //To be set
+int ultSonicThresh = 25;
 long ultDist;
 
 //Infrared Sensor
 const int infraPin = A2;
-int infraThresh = 0; //To be set
+int infraThresh = 0;
 float infraVoltage = 0;
 
 //Phototransistor
 const int photoPin = A3;
-int photoThresh = 0; //To be set
+int photoThresh = 0;
 float photoVoltage = 0;
 
 //Wheel Motors
@@ -21,7 +21,7 @@ int rightMotorDirPin = 13;
 int rightMotorSpeedPin = 11;
 
 //DC Fan Motor
-int fanMotorPin = 4; //To be set
+int fanMotorPin = 4;
 
 void setup() {
   Serial.begin(9600);
@@ -39,8 +39,8 @@ void setup() {
 
 void loop() {
   ultDist = calculateSonicDist(ultSonicOutPin,ultSonicInPin);
-  infraVoltage = (analogRead(infraPin)/1023)*5;
-  photoVoltage = (analogRead(photoPin)/1023)*5; //Is this the right equation?
+  infraVoltage = (analogRead(infraPin)/1023.0)*5.0;
+  photoVoltage = (analogRead(photoPin)/1023.0)*5.0;
   Serial.println(infraPin);
   Serial.println(ultDist);
   Serial.println(infraVoltage);
@@ -51,16 +51,15 @@ void loop() {
       goForward(ldir,lspeed,rdir,rspeed);
     }
     extinguish(leftMotorSpeedPin,rightMotorSpeedPin,fanMotorPin);
-    //If we think that the current extinguish() is fine, then I'll replace this part with the code in the function.
     halt(lspeed,rspeed);
   }
-  else if(infraVoltage<infraThresh){ //Right Empty
+  else if(infraVoltage<infraThresh){ //Right empty
     turnRight(leftMotorDirPin,leftMotorSpeedPin,rightMotorDirPin,rightMotorSpeedPin);
   }
   else if(ultDist>ultSonicThresh){ //Front blocked
     turnLeft(leftMotorDirPin,leftMotorSpeedPin,rightMotorDirPin,rightMotorSpeedPin);
   }
-  else{
+  else{ //Front empty
     goForward(leftMotorDirPin,leftMotorSpeedPin,rightMotorDirPin,rightMotorSpeedPin);
   }
   
@@ -82,14 +81,16 @@ void turnRight(int lmd, int lms, int rmd, int rms){ //This is supposed to be set
   digitalWrite(rms,HIGH);
   digitalWrite(rmd,LOW);
   delay() //Set this for as long as you want it to turn
+  halt(lspeed,rspeed);
 }
 
-void turnLeft(int lmd, int lms, int rmd, int rms){
+void turnLeft(int lmd, int lms, int rmd, int rms){ //This is supposed to be set up to rotate in place
   digitalWrite(rmd,HIGH);
   digitalWrite(rms,HIGH);
   digitalWrite(lms,HIGH);
   digitalWrite(lmd,LOW);
   delay() //Set this for as long as you want it to turn
+  halt(lspeed,rspeed);
 }
 
 void goForward(int ldir, int lspeed, int rdir, int rspeed){
